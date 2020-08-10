b1eced7385337e7901e2ce0fb0d08788a4de0e5d
Karim Abdelgwad <karim.elbadry2@gmail.com>
2020-08-10 10:01:17 +0200

    header

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -41,13 +41,19 @@
             {!!$api['advs']->popup_en!!}
             @endif
         </div>
-        <div class="col-12 px-0">
+        @if($api['advs']->header_status==1 &&  session('header_status')!="false")
             @if(session('language')=="ar")
-                {!!preg_replace('!(((f|ht)tp(s)?://)[-a-zA-Zа-яА-Я()0-9@:%_+.~#?&;//=]+)!i', '<a href="$1">$1</a>', $api['advs']->header_ar)!!}
+                <div class="col-12 px-0"
+                     style="background-color: #EB593C; color: white; text-align: center; padding: 5px; direction: rtl">
+                    {!!preg_replace('!(((f|ht)tp(s)?://)[-a-zA-Zа-яА-Я()0-9@:%_+.~#?&;//=]+)!i', '<a href="$1">$1</a>', $api['advs']->header_ar)!!}
+                </div>
             @else
-                {!!preg_replace('!(((f|ht)tp(s)?://)[-a-zA-Zа-яА-Я()0-9@:%_+.~#?&;//=]+)!i', '<a href="$1">$1</a>', $api['advs']->header_en)!!}
+                <div class="col-12 px-0"
+                     style="background-color: #EB593C; color: white; text-align: center; padding: 5px; direction: ltr">
+                    {!!preg_replace('!(((f|ht)tp(s)?://)[-a-zA-Zа-яА-Я()0-9@:%_+.~#?&;//=]+)!i', '<a href="$1">$1</a>', $api['advs']->header_en)!!}
+                </div>
             @endif
-        </div>
+        @endif
     </div>
 
     <script type="text/javascript">
